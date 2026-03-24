73963316715e6f67e39a8307de361c7aec2372cd
Pet-Garcia <petrusgarcia11@gmail.com>
2026-03-24 16:19:29 -0300

    atualizado os arquivos academico.sql e sqlcodigo-matricula

--- UML/academico.sql
+++ UML/academico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24/03/2026 às 20:05
+-- Tempo de geração: 24/03/2026 às 20:16
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -35,6 +35,17 @@ CREATE TABLE `alunos` (
   `cpf` char(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `alunos`
+--
+
+INSERT INTO `alunos` (`ra`, `nome`, `cpf`) VALUES
+(1001, 'João Pedro', '123.456.789-00'),
+(1002, 'Maria Clara', '234.567.890-11'),
+(1003, 'Lucas Santos', '345.678.901-22'),
+(1004, 'Beatriz Oliveira', '456.789.012-33'),
+(1005, 'Gabriel Rocha', '567.890.123-44');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +56,17 @@ CREATE TABLE `cursos` (
   `idcurso` int(5) NOT NULL,
   `nomecurso` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cursos`
+--
+
+INSERT INTO `cursos` (`idcurso`, `nomecurso`) VALUES
+(1, 'Sistemas de Informação'),
+(2, 'Engenharia de Software'),
+(3, 'Ciência da Computação'),
+(4, 'Análise e Desenvolvimento de Sistemas'),
+(5, 'Banco de Dados');
 
 -- --------------------------------------------------------
 
@@ -59,6 +81,17 @@ CREATE TABLE `disciplina` (
   `idprofessor` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `disciplina`
+--
+
+INSERT INTO `disciplina` (`iddisciplina`, `nomedisciplina`, `idcurso`, `idprofessor`) VALUES
+(1, 'Banco de Dados I', 5, 1),
+(2, 'Programação I', 1, 2),
+(3, 'Estruturas de Dados', 3, 3),
+(4, 'Engenharia de Software I', 2, 4),
+(5, 'Análise de Sistemas', 4, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +103,17 @@ CREATE TABLE `itemturma` (
   `ra` int(16) NOT NULL,
   `idturma` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `itemturma`
+--
+
+INSERT INTO `itemturma` (`iditem`, `ra`, `idturma`) VALUES
+(1, 1001, 1),
+(2, 1002, 2),
+(3, 1003, 3),
+(4, 1004, 4),
+(5, 1005, 5);
 
 -- --------------------------------------------------------
 
@@ -83,6 +127,17 @@ CREATE TABLE `professor` (
   `cpf` char(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `professor`
+--
+
+INSERT INTO `professor` (`idprofessor`, `nomeprofessor`, `cpf`) VALUES
+(1, 'Carlos Silva', '111.111.111-11'),
+(2, 'Ana Souza', '222.222.222-22'),
+(3, 'Marcos Lima', '333.333.333-33'),
+(4, 'Fernanda Costa', '444.444.444-44'),
+(5, 'Juliana Alves', '555.555.555-55');
+
 -- --------------------------------------------------------
 
 --
@@ -92,8 +147,21 @@ CREATE TABLE `professor` (
 CREATE TABLE `turma` (
   `idturma` int(5) NOT NULL,
   `nometurma` varchar(50) NOT NULL,
-  `iddisciplina` int(5) NOT NULL
+  `iddisciplina` int(5) NOT NULL,
+  `semestre` int(3) NOT NULL,
+  `ano` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `turma`
+--
+
+INSERT INTO `turma` (`idturma`, `nometurma`, `iddisciplina`, `semestre`, `ano`) VALUES
+(1, 'Turma A - BD', 1, 1, 2026),
+(2, 'Turma B - Prog', 2, 2, 2026),
+(3, 'Turma C - ED', 3, 1, 2025),
+(4, 'Turma D - ES', 4, 2, 2025),
+(5, 'Turma E - AS', 5, 1, 2026);
 
 --
 -- Índices para tabelas despejadas
@@ -148,19 +216,19 @@ ALTER TABLE `turma`
 -- AUTO_INCREMENT de tabela `disciplina`
 --
 ALTER TABLE `disciplina`
-  MODIFY `iddisciplina` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `iddisciplina` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `itemturma`
 --
 ALTER TABLE `itemturma`
-  MODIFY `iditem` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `iditem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `idprofessor` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `idprofessor` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para tabelas despejadas
